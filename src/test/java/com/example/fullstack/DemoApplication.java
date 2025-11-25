88857043fd6da62978bcb59fc74681af38a22ff2
abdelghani-Moussaouali <elmiss3ali2004@gmail.com>
2025-11-25 12:45:36 +0100

    lasting

--- src/test/java/com/example/fullstack/DemoApplication.java
+++ src/test/java/com/example/fullstack/DemoApplication.java
@@ -1,11 +1,10 @@
 package com.example.fullstack;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class DemoTestApplicationTests {
+@Test
+void contextLoads() {
+}
 }
